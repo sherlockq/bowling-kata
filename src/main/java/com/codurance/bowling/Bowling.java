ceb430f3f0807cbabd76ed5749930ac0e9190eb2
enricjorda <enric.jorda@codurance.com>
2019-09-09 17:21:27 +0200

    add the slash

--- src/main/java/com/codurance/bowling/Bowling.java
+++ src/main/java/com/codurance/bowling/Bowling.java
@@ -1,14 +1,25 @@
 package com.codurance.bowling;
 
 public class Bowling {
+
     private boolean digit;
 
     public int score(String input) {
         int score = 0;
-        for(char oneChar : input.toCharArray()){
+        int prevValue= 0;
+
+        for (char oneChar : input.toCharArray()) {
+            int valueOfCurrentChar = 0;
             if (Character.isDigit(oneChar)) {
-                score += Character.getNumericValue(oneChar);
+                valueOfCurrentChar = Character.getNumericValue(oneChar);
+            } else {
+                if (oneChar == '/') {
+                    valueOfCurrentChar = 10 - prevValue;
+                }
             }
+
+            score += valueOfCurrentChar;
+            if(valueOfCurrentChar != 0) prevValue = valueOfCurrentChar;
         }
         return score;
     }
