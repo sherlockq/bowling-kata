0d22c43a0bd1952a86a3b0c005304d52117ffca7
Sherlock Qiao <sherlockq@gmail.com>
2019-09-10 16:46:52 +0100

    done

--- src/main/java/com/codurance/bowling/Bowling.java
+++ src/main/java/com/codurance/bowling/Bowling.java
@@ -8,33 +8,56 @@ public class Bowling {
 
     public int score(String input) {
         ballChars = input.replace("|", "").toCharArray();
+
         int ballIndex = 0;
         int score = 0;
-        for (int frame = 0; frame < TOTAL_FRAME; frame++) {
-            if(ballChars[ballIndex] == 'X') {
-                score += 10 + getBallValue(ballIndex + 1) + getBallValue(ballIndex + 2);
-                ballIndex += 1;
-            }
-            else if(ballChars[ballIndex + 1 ] == '/') {
-                score += 10 + getBallValue(ballIndex + 2);
-                ballIndex += 2;
-            } else {
-                score += getBallValue(ballIndex) + getBallValue(ballIndex + 1);
-                ballIndex += 2;
-            }
 
+        for (int frame = 0; frame < TOTAL_FRAME; frame++) {
+            if (isStrikeFrame(ballIndex)) {
+                score += 10 + strikeBonus(ballIndex);
+                ballIndex += 1;
+
+            } else if (isSpareFrame(ballIndex)) {
+                score += 10 + spareBonus(ballIndex);
+                ballIndex += 2;
+
+            } else {
+                score += sumOfBalls(ballIndex);
+                ballIndex += 2;
+
+            }
         }
         return score;
     }
 
-    int getBallValue(int index) {
+    private int sumOfBalls(int ballIndex) {
+        return getBallValue(ballIndex) + getBallValue(ballIndex + 1);
+    }
+
+    private int spareBonus(int ballIndex) {
+        return getBallValue(ballIndex + 2);
+    }
+
+    private int strikeBonus(int ballIndex) {
+        return getBallValue(ballIndex + 1) + getBallValue(ballIndex + 2);
+    }
+
+    private boolean isSpareFrame(int ballIndex) {
+        return ballChars[ballIndex + 1] == '/';
+    }
+
+    private boolean isStrikeFrame(int ballIndex) {
+        return ballChars[ballIndex] == 'X';
+    }
+
+    private int getBallValue(int index) {
         char ballChar = ballChars[index];
         if (Character.isDigit(ballChar)) {
             return Character.getNumericValue(ballChar);
         }
         switch (ballChar) {
             case '/':
-                return 10 - getBallValue(index-1);
+                return 10 - getBallValue(index - 1);
             case 'X':
                 return 10;
         }
